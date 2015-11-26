a95569a3c53bb50b744bd6352479948a1ad2d5a1
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-26 18:17:20 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
+var _ = require('lodash');
+var SlackClient = require('slack-api-client');
+
+var pickResult = {
+    'ok': 'ok'
+};
+
 module.exports = {
     /**
-     * The main entry point for the Dexter module
+     * Run main twitter function.
+     *
+     * @param params
+     * @param callback
+     */
+    slackMain: function (params, callback) {
+        var slack = new SlackClient(params.token);
+
+        // Follow befriended
+        slack.api.groups.leave(_.omit(params, 'token'), callback);
+    },
+
+    /**
+     * Return pick result.
+     *
+     * @param output
+     * @returns {*}
+     */
+    pickResult: function (output) {
+        var result = {};
+
+        _.map(_.keys(pickResult), function (val) {
+
+            if (_.has(output, val)) {
+
+                _.set(result, pickResult[val], _.get(output, val));
+            }
+        });
+
+        return result;
+    },
+
+    /**
+     * Allows the authenticating users to follow the user specified in the ID parameter.
      *
      * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.slackMain(step.inputs(), function (error, apiResult) {
+            if (error) {
+                // if error - send message
+                this.fail(error);
+            }
+            // return befriendedInfo
+            this.complete(this.pickResult(apiResult));
+        }.bind(this));
     }
 };
