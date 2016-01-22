9a246a4c26437c5cfdb2196a5e3ff6b46b7aea8d
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-22 20:49:56 +0200

    fix

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-var _ = require('lodash');
-var SlackClient = require('slack-api-client');
+var util = require('./util.js'),
+    _ = require('lodash'),
+    SlackClient = require('slack-node');
 
-var pickResult = {
-    'ok': 'ok'
-};
+var pickInputs = {
+        'chanel': 'chanel'
+    },
+    pickOutputs = {
+        'ok': 'ok'
+    };
 
 module.exports = {
-    /**
-     * Run main twitter function.
-     *
-     * @param params
-     * @param callback
-     */
-    slackMain: function (params, callback) {
-        var slack = new SlackClient(params.token);
-
-        // Follow befriended
-        slack.api.groups.leave(_.omit(params, 'token'), callback);
-    },
-
-    /**
-     * Return pick result.
-     *
-     * @param output
-     * @returns {*}
-     */
-    pickResult: function (output) {
-        var result = {};
-
-        _.map(_.keys(pickResult), function (val) {
-
-            if (_.has(output, val)) {
-
-                _.set(result, pickResult[val], _.get(output, val));
-            }
-        });
-
-        return result;
-    },
-
     /**
      * Allows the authenticating users to follow the user specified in the ID parameter.
      *
@@ -46,14 +17,19 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.provider('slack').credentials('access_token'),
+            slack = new SlackClient(token);
 
-        this.slackMain(step.inputs(), function (error, apiResult) {
-            if (error) {
-                // if error - send message
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        slack.api.groups.leave(inputs, function (error, data) {
+            if (error)
                 this.fail(error);
-            }
-            // return befriendedInfo
-            this.complete(this.pickResult(apiResult));
+            else
+                this.complete(util.pickOutputs(data, pickOutputs));
         }.bind(this));
     }
 };
